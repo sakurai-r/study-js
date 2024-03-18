a1dd7c1ff334d5cfada3b231e26c73469a643edb
RFG\r00527802 <rikuto.sakurai@jp.ricoh.com>
2024-03-18 19:25:46 +0900

    ex01

---
+++ ch03/ex01/index.ts
@@ -0,0 +1,32 @@
+/**
+ * Infinity + Infinity = Infinity
+ * Infinity - Infinity = NaN
+ * Infinity * Infinity = Infinity
+ * Infinity / Infinity = NaN
+ */
+console.log(`Infinity + Infinity = ${Infinity + Infinity}`);
+console.log(`Infinity - Infinity = ${Infinity - Infinity}`);
+console.log(`Infinity * Infinity = ${Infinity * Infinity}`);
+console.log(`Infinity / Infinity = ${Infinity / Infinity}`);
+
+/**
+ * -Infinity + (-Infinity) = -Infinity
+ * -Infinity - (-Infinity) = NaN
+ * -Infinity * (-Infinity) = Infinity
+ * -Infinity / (-Infinity) = NaN
+ */
+console.log(`-Infinity + (-Infinity) = ${-Infinity + -Infinity}`);
+console.log(`-Infinity - (-Infinity) = ${-Infinity - -Infinity}`);
+console.log(`-Infinity * (-Infinity) = ${-Infinity * -Infinity}`);
+console.log(`-Infinity / (-Infinity) = ${-Infinity / -Infinity}`);
+
+/**
+ * NaN + NaN = NaN
+ * NaN - NaN = NaN
+ * NaN * NaN = NaN
+ * NaN / NaN = NaN
+ */
+console.log(`NaN + NaN = ${NaN + NaN}`);
+console.log(`NaN - NaN = ${NaN - NaN}`);
+console.log(`NaN * NaN = ${NaN * NaN}`);
+console.log(`NaN / NaN = ${NaN / NaN}`);
