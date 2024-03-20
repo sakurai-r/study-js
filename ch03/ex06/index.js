09ee8412622eb1e676f39efe45fb1b240bbb47cd
sakurai-r <rikuto.sakurai@jp.ricoh.com>
2024-03-21 08:18:31 +0900

    wip

--- ch03/ex06/index.js
+++ ch03/ex06/index.js
@@ -1,6 +1,15 @@
 export function substring(str, indexStart, indexEnd) {
-  // TODO: ここを実装しなさい
-  return "TODO";
+  let result = "";
+  if (indexEnd === undefined) {
+    for (let i = indexStart; i < str.length; i++) {
+      result += str[i];
+    }
+  } else {
+    for (let i = indexStart; i < indexEnd; i++) {
+      result += str[i];
+    }
+  }
+  return result;
 }
 
 export function slice(str, indexStart, indexEnd) {
