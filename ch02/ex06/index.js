361969276acde24f7467621dd94be5328da40e11
sakurai-r <rikuto.sakurai@jp.ricoh.com>
2024-03-23 15:30:55 +0900

    fix

--- ch02/ex06/index.js
+++ ch02/ex06/index.js
@@ -15,7 +15,7 @@ export const fizzbuzz = ()=>Array.from({length:100},(_, i) =>(i+1)%15?(i+1)%3?(i
 
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Conditional_operator#%E6%9D%A1%E4%BB%B6%E3%81%AE%E9%80%A3%E9%8E%96
 /**
- * 三項演算子は右結合で、すなわち以下のような方法で if … else if … else if … else の連鎖と同様に「連鎖」させることができます。
+ * 三項演算子は右結合で、すなわち以下のような方法で if … else if … else if … else の連鎖と同様に「連鎖」させることができます。
  */
 
 //function example(…) {
