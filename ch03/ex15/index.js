48a95e85ebe82ad52adb6d0729b35af33c2da8c8
sakurai-r <rikuto.sakurai@jp.ricoh.com>
2024-03-31 22:04:29 +0900

    ex15

--- ch03/ex15/index.js
+++ ch03/ex15/index.js
@@ -1,28 +1,27 @@
 /* eslint-disable */
-//for (let i = 0; i < 10; i++) {
-//  (function () {
-//    let i = 100;
-//  })();
-//  console.log(i);
-//}
-//console.log(i);
-
-/* eslint-disable */
-//for (var i = 0; i < 10; i++) {
-//  (function () {
-//    var i = 100;
-//  })();
-//  console.log(i);
-//}
-//console.log(i);
-
-/* eslint-disable */
-(function () {
-  for (i = 0; i < 10; i++) {
-    (function () {
-      i = 100;
-    })();
-    console.log(i);
-  }
+"use strict";
+for (let i = 0; i < 10; i++) {
+  (function () {
+    let i = 100;
+  })();
   console.log(i);
-})();
+}
+console.log(i);
+
+/* eslint-disable */
+for (var i = 0; i < 10; i++) {
+  (function () {
+    var i = 100;
+  })();
+  console.log(i);
+}
+console.log(i);
+
+// eslint-disable
+//for (i = 0; i < 10; i++) {
+//  (function () {
+//    i = 100;
+//  })();
+//  console.log(i);
+//}
+//console.log(i);
