4bdf17717e8d316299480c1a4fc6915f89ca8dd0
sakurai-r <rikuto.sakurai@jp.ricoh.com>
2024-06-04 20:51:08 +0900

    ex03

--- ch07/ex03/index.js
+++ ch07/ex03/index.js
@@ -5,15 +5,28 @@ export const sum = (array) => {
   return array.reduce((x, y) => x + y, 0);
 };
 
-export const join = (array, _separator) => {
-  const separator = _separator ? _separator : ",";
-  return array.reduce((x, y) => {
-    y ? x + separator + y : "";
-  }, "");
+export const join = (array, separator = ",") => {
+  if (!Array.isArray(array)) {
+    throw new Error();
+  }
+
+  if (array.length === 0) {
+    return "";
+  }
+
+  // Null 合体演算子 左辺が null または undefined の場合に右の値を返す
+  // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
+  return array.reduce((x, y) => `${x ?? ""}${separator}${y ?? ""}`);
 };
 
-export const reverse = (array) => {};
+export const reverse = (array) => {
+  return array.reduce((x, y) => [y, ...x], []);
+};
 
-export const every = () => {};
+export const every = (array, callback) => {
+  return array.reduce((x, y, i, arr) => x && callback(y, i, arr), true);
+};
 
-export const some = () => {};
+export const some = (array, callback) => {
+  return array.reduce((x, y, i, arr) => x || callback(y, i, arr), false);
+};
