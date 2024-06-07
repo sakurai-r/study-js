ef1cc1d7c7bd066e0b29a2ff0a8b46295084b5b8
sakurai-r <rikuto.sakurai@jp.ricoh.com>
2024-06-07 10:47:39 +0900

    fix

--- ch06/ex03/index.js
+++ ch06/ex03/index.js
@@ -10,20 +10,6 @@ q.z = 3; // 独自プロパティz を持つ。
 console.log(`p.isPrototypeOf(q) = ${p.isPrototypeOf(q)}`);
 console.log(`o.isPrototypeOf(q) = ${o.isPrototypeOf(q)}`);
 
-// `Array`はコンストラクタ関数であり、関数オブジェクトである。false
-// 関数オブジェクトのプロトタイプは`Function.prototype`であり、`Object.prototype`は直接のプロトタイプではないのでfalseになる。
-// ただし、`Function.prototype`は`Object.prototype`を継承している。
-console.log(
-  `Object.prototype.isPrototypeOf(Array) = ${Object.prototype.isPrototypeOf(
-    Array
-  )}`
-); // -> false
-console.log(
-  `Object.prototype.isPrototypeOf(Function) = ${Object.prototype.isPrototypeOf(
-    Function
-  )}`
-); // -> false
-
 // `Array.prototype`オブジェクト(Arrayのインスタンスのプロトタイプ)が
 // Object.prototypeのプロトタイプチェーンに含まれているかを確認している
 console.log(
