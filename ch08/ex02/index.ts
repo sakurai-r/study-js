4b7d231223610d328fbcc708c9359b8fcffeb46c
sakurai-r <rikuto.sakurai@jp.ricoh.com>
2024-06-10 22:29:20 +0900

    ex03

--- ch08/ex02/index.ts
+++ ch08/ex02/index.ts
@@ -7,12 +7,8 @@ export const pow = (x: number, n: number) => {
     n = -n;
   }
   let result = 1;
-  while (n > 0) {
-    if (n % 2 === 1) {
-      result *= x;
-    }
-    x *= x;
-    n = Math.floor(n / 2);
+  for (let i = 0; i < n; i++) {
+    result *= x;
   }
   return result;
 };
@@ -24,10 +20,5 @@ export const powerRecursive = (x: number, n: number): number => {
   if (n < 0) {
     return 1 / powerRecursive(x, -n);
   }
-  if (n % 2 === 0) {
-    const halfPower = powerRecursive(x, n / 2);
-    return halfPower * halfPower;
-  } else {
-    return x * powerRecursive(x, n - 1);
-  }
+  return x * powerRecursive(x, n - 1);
 };
