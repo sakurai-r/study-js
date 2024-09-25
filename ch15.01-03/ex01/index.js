ff897a96b311d164257959b08b5230b41c24a9f9
sakurai-r <rikuto.sakurai@jp.ricoh.com>
2024-09-26 00:25:26 +0900

    ex01

--- ch15.01-03/ex01/index.js
+++ ch15.01-03/ex01/index.js
@@ -4,6 +4,16 @@ const input = document.querySelector("#new-todo");
 
 form.addEventListener("submit", (e) => {
   // TODO: ここで form のイベントのキャンセルを実施しなさい (なぜでしょう？)
+  /**
+   * event.preventDefaultメソッドは、submitイベントの発生元であるフォームが持つ
+   * デフォルトの動作をキャンセルするメソッドです。
+   * フォームが持つデフォルトの動作とは、フォームの内容を指定したURLへ送信するという動作です。
+   * ここではform要素に送信先が指定されていないため、現在のURLに対してフォームの内容を送信します。
+   * しかしこの動作は邪魔になるため、event.preventDefaultメソッドを呼び出すことで、
+   * このデフォルトの動作をキャンセルしています。
+   * https://jsprimer.net/use-case/todoapp/form-event/#form-event
+   */
+  e.preventDefault();
 
   // 両端からホワイトスペースを取り除いた文字列を取得する
   if (input.value.trim() === "") {
@@ -21,11 +31,18 @@ form.addEventListener("submit", (e) => {
   label.style.textDecorationLine = "none";
 
   const toggle = document.createElement("input");
+  toggle.type = "checkbox";
   // TODO: toggle が変化 (change) した際に label.style.textDecorationLine を変更しなさい
-
+  toggle.onchange = (e) => {
+    label.style.textDecorationLine = e.target.checked ? "line-through" : "none";
+  };
   const destroy = document.createElement("button");
+  destroy.innerText = "❌";
   // TODO: destroy がクリック (click) された場合に elem を削除しなさい
-
+  destroy.onclick = () => {
+    elem.remove();
+  };
   // TODO: elem 内に toggle, label, destroy を追加しなさい
+  elem.append(toggle, label, destroy);
   list.prepend(elem);
 });
