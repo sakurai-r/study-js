36bc7ca5a469eae46dbc4aee5697e7a7d01034f5
sakurai-r <rikuto.sakurai@jp.ricoh.com>
2024-11-04 17:15:47 +0900

    fix

--- ch15.04-10/ex10/index.js
+++ ch15.04-10/ex10/index.js
@@ -47,6 +47,47 @@ function updateGrid(grid) {
   for (let row = 0; row < ROWS; row++) {
     for (let col = 0; col < COLS; col++) {
       // 周囲のセルの生存数を数えて nextGrid[row][col] に true or false を設定する (実装してね)
+      let countTrue = 0;
+      // 上のセル
+      if (row > 0 && grid[row - 1][col]) {
+        countTrue++;
+      }
+      // 下のセル
+      if (row < ROWS - 1 && grid[row + 1][col]) {
+        countTrue++;
+      }
+      // 左のセル
+      if (col > 0 && grid[row][col - 1]) {
+        countTrue++;
+      }
+      // 右のセル
+      if (col < COLS - 1 && grid[row][col + 1]) {
+        countTrue++;
+      }
+      // 左上のセル
+      if (row > 0 && col > 0 && grid[row - 1][col - 1]) {
+        countTrue++;
+      }
+      // 右上のセル
+      if (row > 0 && col < COLS - 1 && grid[row - 1][col + 1]) {
+        countTrue++;
+      }
+      // 左下のセル
+      if (row < ROWS - 1 && col > 0 && grid[row + 1][col - 1]) {
+        countTrue++;
+      }
+      // 右下のセル
+      if (row < ROWS - 1 && col < COLS - 1 && grid[row + 1][col + 1]) {
+        countTrue++;
+      }
+
+      if (grid[row][col]) {
+        // 隣り合うセルの true がちょうど 2 か 3 の場合は、true になる
+        nextGrid[row][col] = countTrue === 2 || countTrue === 3;
+      } else {
+        // 隣り合うセルの true がちょうど 3 の場合は、true になる
+        nextGrid[row][col] = countTrue === 3;
+      }
     }
   }
   return nextGrid;
