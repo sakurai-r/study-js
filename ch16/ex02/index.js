9c94c725272a452f1b24a5e225d996fe93460673
sakurai-r <rikuto.sakurai@jp.ricoh.com>
2024-12-29 16:00:43 +0900

    ex02

--- ch16/ex02/index.js
+++ ch16/ex02/index.js
@@ -31,3 +31,39 @@ async function startChild() {
 }
 
 // TODO: ここに処理を書く
+
+// https://nodejs.org/docs/latest-v14.x/api/process.html#process_signal_events
+// シグナルをトラップし、子プロセスに転送して終了を待つ
+process.on("SIGINT", () => {
+  console.log("Receive SIGINT.");
+  if (child) {
+    child.kill("SIGINT");
+    child.on("close", () => {
+      process.exit(0);
+    });
+  } else {
+    process.exit(0);
+  }
+});
+
+process.on("SIGTERM", () => {
+  console.log("Receive SIGTERM.");
+  if (child) {
+    child.kill("SIGTERM");
+    child.on("close", () => {
+      process.exit(0);
+    });
+  } else {
+    process.exit(0);
+  }
+});
+
+
+while (true) {
+  const result = await startChild();
+  if (result.code === 0) {
+    console.log("Child process exited normally.");
+    break;
+  }
+  console.log(`Child process exited with code ${result.code}. Restarting...`);
+}
