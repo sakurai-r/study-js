1c95432d5e94c10d1c8966e5f72118851ba64e43
sakurai-r <rikuto.sakurai@jp.ricoh.com>
2025-01-06 23:21:49 +0900

    ch13

---
+++ ch16/ex13/index.md
@@ -0,0 +1,30 @@
+# Linux
+> echo HELLO | tr [:upper:] [:lower:] > hello.txt
+
+# Windows (WSL が必要)
+> wsl echo HELLO | wsl tr [:upper:] [:lower:] > hello.txt
+
+# いずれも hello.txt に `hello` と書き込まれる
+
+# Windows (WSL 不要)
+# 問題: なぜ直接 dir を使わず cmd /c を書いているのだろうか？これらの意味は？
+> cmd /c dir | cmd /c "findstr DIR"
+
+`dir`コマンドは、Windowsの内部コマンドであり、外部プログラムではない。そのため、Node.jsのchild_process.spawnやexecは`dir`を直接実行しようとするとエラーになる。
+`cmd /c`を使うことで、`dir`などの内部コマンドをcmdプロセスの中で実行させることができる。これにより、`dir`のような外部ファイルとして存在しないコマンドも実行可能になる。
+
+```sh
+>  dir | "findstr DIR"
+Error: spawn dir ENOENT
+    at ChildProcess._handle.onexit (node:internal/child_process:284:19)
+    at onErrorNT (node:internal/child_process:477:16)
+    at process.processTicksAndRejections (node:internal/process/task_queues:82:21) {
+  errno: -4058,
+  code: 'ENOENT',
+  syscall: 'spawn dir',
+  path: 'dir',
+  spawnargs: []
+}
+```
+
+
--- ch16/ex13/shell.js
+++ ch16/ex13/shell.js
@@ -69,6 +69,8 @@ async function runcmd(cmd, stdin = null, stdout = null) {
       {
         // FIXME: ここを実装してね (2行程度)
         // HINT: cmd.file のストリームを createWriteStream で作成し runcmd を再帰的に呼び出す
+        const leftStream = fs.createWriteStream(cmd.file);
+        await runcmd(cmd.cmd, stdin, leftStream);
       }
       break;
 
@@ -76,6 +78,8 @@ async function runcmd(cmd, stdin = null, stdout = null) {
       {
         // FIXME: ここを実装してね (2行程度)
         // HINT: cmd.file のストリームを createReadStream で作成し runcmd を再帰的に呼び出す
+        const rightStream = fs.createReadStream(cmd.file);
+        await runcmd(cmd.cmd, rightStream, stdout);
       }
       break;
 
@@ -84,6 +88,11 @@ async function runcmd(cmd, stdin = null, stdout = null) {
         // FIXME: ここを実装してね (4行程度)
         // HINT: cmd.left と cmd.right に対して runcmd を再帰的に呼び出し Promise.all で待つ
         // HINT: left と right を繋ぐには new PassThrought() で作成したストリームを使用する
+        const pipeStream = new PassThrough();
+        await Promise.all([
+          runcmd(cmd.left, stdin, pipeStream),
+          runcmd(cmd.right, pipeStream, stdout),
+        ]);
       }
       break;
 
