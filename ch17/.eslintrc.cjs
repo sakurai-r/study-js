8c35154853d5c47032217025e48a0c6c6292a6e4
sakurai-r <rikuto.sakurai@jp.ricoh.com>
2025-01-23 23:06:43 +0900

    ex01

--- ch17/.eslintrc.cjs
+++ ch17/.eslintrc.cjs
@@ -12,5 +12,12 @@ module.exports = {
   },
   rules: {
     'prettier/prettier': ['warn'],
+    'require-jsdoc': 'off', // JSDocコメントを要求しない
+    'no-unused-vars': 'off', // 未使用変数の警告を無効化
+    '@typescript-eslint/no-unused-vars': 'off', // TypeScriptの未使用変数の警告を無効化
+    'no-invalid-this': 'off', // thisの無効使用の警告を無効化
+    'no-undef': 'off', // 未定義変数の使用の警告を無効化
+    'no-redeclare': 'off', // 変数の再定義の警告を無効化
+    'no-empty': 'off', // 空のブロックの警告を無効化
   },
 };
