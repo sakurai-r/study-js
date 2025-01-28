e9ffa63792143202fd5c9b55fdbd5d75261fab7d
sakurai-r <rikuto.sakurai@jp.ricoh.com>
2025-01-28 18:48:57 +0900

    ex06

--- ch17/webpack.config.cjs
+++ ch17/webpack.config.cjs
@@ -1,12 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: './ex05/index.js', // エントリーポイント
+  entry: './ex06/index.js',
   output: {
     filename: 'bundle.js', // バンドル後のファイル名
-    path: path.resolve(__dirname, 'ex05/dist'), // 出力先ディレクトリ
+    path: path.resolve(__dirname, 'ex06/dist'), // 出力先ディレクトリ
   },
   mode: 'production',
+  devtool: 'source-map',
   module: {
     rules: [
       {
