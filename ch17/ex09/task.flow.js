41d3846a217973e82b25bbf7f5756f8af9f474e7
sakurai-r <rikuto.sakurai@jp.ricoh.com>
2025-01-31 00:07:02 +0900

    ex09

--- ch17/ex09/task.flow.js
+++ ch17/ex09/task.flow.js
@@ -6,13 +6,16 @@
 
 // @flow
 
-type User = {
-  id: number,
-  name: string,
-};
+type User = { id: number, name: string };
+
+type Task = { title: string, completed: boolean, user: User };
+
+type Priority = 'low' | 'middle' | 'high';
+
+type PriorityTask = Task & { priority: Priority };
 
 // Userオブジェクトであることを判定する
-function isUserObject(obj) {
+function isUserObject(obj: any): boolean {
   return (
     typeof obj === 'object' &&
     typeof obj['id'] === 'number' &&
@@ -21,17 +24,17 @@ function isUserObject(obj) {
 }
 
 export class TaskManager {
-  _tasks = [];
+  _tasks: Array<PriorityTask> = [];
 
   // タスクを追加する
-  add(task) {
+  add(task: PriorityTask): void {
     this._tasks.push(task);
   }
 
   // タスクを完了にする
   // Userオブジェクトを指定した場合はそのUserのタスクを全て完了にする
   // 文字列を指定した場合は、そのタイトルのタスクを全て完了にする
-  completeTask(target) {
+  completeTask(target: User | string): void {
     if (isUserObject(target)) {
       this._tasks
         .filter((t) => t.user === target)
@@ -45,7 +48,7 @@ export class TaskManager {
 
   // 引数の関数にマッチするタスクを返す
   // 引数を省略した場合はすべてのタスクを返す
-  getTasks(predicate) {
+  getTasks(predicate?: (task: PriorityTask) => boolean): Array<PriorityTask> {
     if (predicate === undefined) {
       return this._tasks;
     } else {
@@ -55,11 +58,13 @@ export class TaskManager {
 }
 
 // priority="low"または完了済のタスクを判定する
-export function isLowOrCompletedTask(priorityTask) {
+export function isLowOrCompletedTask(priorityTask: PriorityTask): boolean {
   return priorityTask.priority === 'low' || priorityTask.completed;
 }
 
 // 判定関数の否定結果を返す関数を生成する
-export function not(f) {
+export function not(
+  f: (arg: PriorityTask) => boolean,
+): (arg: PriorityTask) => boolean {
   return (arg) => !f(arg);
 }
